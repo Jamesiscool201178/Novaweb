055f23db13f9d1607688a1d246e38beb6e26a15d
Jamesiscool201178 <jameshuby748@gmail.com>
2026-09-24 20:56:19 +0100

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,7 +0,0 @@
-const SUPABASE_URL = "https://ogjzdsoyndxtbtnxiypf.supabase.co";
-const SUPABASE_KEY = "sb_publishable_xwPHXgqxGRbqGcTUjrIMMA_jAdVhXBg";
-
-const supabaseClient = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
-);
